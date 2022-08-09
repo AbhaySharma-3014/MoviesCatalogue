8650918fcfca9c02b29fd9d6c89a4808e23c4978
jusforgit <justforgames3014@gmail.com>
2022-08-09 22:59:40 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+
+const Header = () => {
+  return (
+    <span onClick={() => window.scroll(0, 0)} className="header">
+    🎞️      Movies Station     🎬  
+    </span>
+  );
+
+      return (<img src='./logo.png' alt="Logo" />);
+};
+
+
+export default Header;
